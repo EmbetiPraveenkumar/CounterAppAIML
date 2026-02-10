e831cd14bae5c20d6dad84b1e802b90cce6af96a
EmbetiPraveenKumar <praveen.e99999@gmail.com>
2026-02-10 10:09:48 +0530

    this is counter app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>count:{count}</h1>
+        <div>
+          <button onClick={()=>setCount(count+1)}>Increment</button>
+          <button onClick={()=>setCount(count-1)}>Decrement</button>
+          <button onClick={()=>setCount(0)}>Reset</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
